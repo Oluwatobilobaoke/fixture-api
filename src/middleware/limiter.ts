0939ce7e641e318bd00f6b7c726f252932062e4d
pheranmie <okekunletobi@gmail.com>
2024-09-30 23:57:33 +0100

    worked on limiter

--- src/middleware/limiter.ts
+++ src/middleware/limiter.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 const userRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10, // limit each user to 100 requests per windowMs
+  max: 100, // limit each user to 100 requests per windowMs
   keyGenerator: (req) => {
     return req.session?.user?._id || req.ip;
   },
